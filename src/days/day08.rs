895c48f1454bdf4ecd4116ef3a69b7f1488fafd9
Bea Boxler <bea@boxler.me>
2025-03-31 17:02:26 +0100

    2025 Day 8 part 2

--- src/days/day08.rs
+++ src/days/day08.rs
@@ -42,20 +42,26 @@ fn parse_input(input: &str) -> (Ledger, Vec<Instruction>) {
         ledger.insert(person.into(), amount.parse::<Number>().unwrap());
     }
 
-    let instructions = instructions.lines().map(|line| line.parse().unwrap()).collect();
+    let instructions = instructions
+        .lines()
+        .map(|line| line.parse().unwrap())
+        .collect();
 
     (ledger, instructions)
-
 }
 
 fn part_one(input: &str) -> Number {
     let (mut ledger, instructions) = parse_input(input);
 
-    for Instruction{from, to, amount} in instructions {
-        {let from = ledger.get_mut(&from).unwrap();
-        *from -= amount;}
-        {let to = ledger.get_mut(&to).unwrap();
-        *to += amount;}
+    for Instruction { from, to, amount } in instructions {
+        {
+            let from = ledger.get_mut(&from).unwrap();
+            *from -= amount;
+        }
+        {
+            let to = ledger.get_mut(&to).unwrap();
+            *to += amount;
+        }
     }
 
     let mut values = ledger.values().collect::<Vec<_>>();
@@ -65,8 +71,31 @@ fn part_one(input: &str) -> Number {
     values[..3].iter().map(|v| **v).sum()
 }
 
-fn part_two(_input: &str) -> Number {
-    0
+fn part_two(input: &str) -> Number {
+    let (mut ledger, instructions) = parse_input(input);
+
+    for Instruction {
+        from,
+        to,
+        mut amount,
+    } in instructions
+    {
+        {
+            let from = ledger.get_mut(&from).unwrap();
+            amount = amount.min(*from);
+            *from -= amount;
+        }
+        {
+            let to = ledger.get_mut(&to).unwrap();
+            *to += amount;
+        }
+    }
+
+    let mut values = ledger.values().collect::<Vec<_>>();
+    values.sort();
+    values.reverse();
+
+    values[..3].iter().map(|v| **v).sum()
 }
 
 fn part_three(_input: &str) -> Number {
@@ -102,7 +131,7 @@ FROM Charlie TO Delta AMT 302";
 
     #[test]
     fn part_two_returns_correct_output() {
-        assert_eq!(part_two(INPUT), 0);
+        assert_eq!(part_two(INPUT), 2542);
     }
 
     #[test]
